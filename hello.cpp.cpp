fe1908e1694b5f82b328f4c6ed71cd859253184f
dadon2k2k <modou.jammeh01@gmail.com>
2026-09-10 22:38:28 -0400

    Add files via upload

---
+++ hello.cpp.cpp
@@ -0,0 +1,15 @@
+/*Momodou Jammeh    Assignment1*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    
+    cout << "Hello World!" << endl;
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "nice to meet you," << name << "!" << endl;
+
+    return 0;
+}
